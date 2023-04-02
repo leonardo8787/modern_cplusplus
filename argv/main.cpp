1464ff073bb7a358fd8ab4a18de4b7dd4772de90
leonardo8787 <leocampos8787@gmail.com>
2023-04-02 16:58:55 -0300

    atualiza

--- argv/main.cpp
+++ argv/main.cpp
@@ -1,12 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void handler(int signum) {
-    cout << "Para encerrar, pressione Ctrl + C novamente" << endl;
-    signal(SIGINT, SIG_DFL);
-}
-
 int main(int argc, char** argv){
+    int precisao=0;
     default_random_engine generator(time(nullptr));
     pair<double, double> range = {0, 1000};
     string input;
@@ -16,43 +12,40 @@ int main(int argc, char** argv){
         input+=argv[i];
         input.append(" ");
     }
-    cout<<"\nsaida: "<<input<<endl;
+    for(int i=0; i<argc; ++i){
+        if((argv[i]) == "-p"){
+            cout<<"precisão: "<<argv[++i]<<endl;
+            precisao=stoi(argv[++i]);
+        }
+    }
     input.erase(std::remove_if(input.begin(), input.end(), [](unsigned char c){ return std::isspace(c); }), input.end());
     for (int i = 1; i < argc; i++) {
         if (string(argv[i]) == "-i") {
             inteiro = true;
-        } else if (string(argv[i]) == "-n") {
-            n = stoi(argv[++i]);
-        } else if (string(argv[i]) == "-r") {
+        }else if (string(argv[i]) == "-n") {
+            n=stoi(argv[++i]);
+        }else if (string(argv[i]) == "-r") {
             range.first = stod(argv[++i]);
             range.second = stod(argv[++i]);
-        } else if (string(argv[i]) == "-p") {
-            int decimal_places = stoi(argv[++i]);
         } else if (string(argv[i]) == "-s") {
             generator.seed(stoi(argv[++i]));
         }
     }
+    cout<<"precisao: "<<precisao<<endl;
     uniform_real_distribution<double> real_distribution(range.first, range.second);
     uniform_int_distribution<int> int_distribution((int)range.first, (int)range.second);
-    int decimal_places = 0;
-    if(inteiro == true){
+    if(inteiro == true)
         cout<<"Range de numeros inteiros:"<<endl;
-    }else{
+    else
         cout<<"Range de numeros reais:"<<endl;
-    }
     for(int i=0; i<n; i++){
         double value;
         if(inteiro)
             value = int_distribution(generator);
-        else {
+        else 
             value = real_distribution(generator);
-            if (decimal_places > 0) {
-                value = round(value*pow(10, decimal_places))/pow(10, decimal_places);
-            }
-        }
-        cout<<"["<<value<<"] ";
+        cout << fixed;
+        cout<<"["<<setprecision(precisao)<<value<<"] ";
     }
-    signal(SIGINT, handler);
-    while (true){}
     return 0;
 }
